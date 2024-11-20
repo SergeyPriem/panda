a327f6fa7a677edbc320075ea5b38cf4403291b5
Main User <s.priemshiy@gmail.com>
2024-11-20 11:09:17 +0500

    411.4.5

--- iec.py
+++ iec.py
@@ -4,7 +4,7 @@ def ptotection_rule():
 
     st.text("60364-4-41 © IEC:2005")
     st.text("""411.4.4 The characteristics of the protective devices (see 411.4.5) and the circuit  impedances shall fulfil the following requirement:""")
-    st.latex(r'Z_s * I_a \leq U_o')
+    st.latex(r'Z_S * I_a \leq U_o')
     st.text("""where:
     
     Zs is the impedance in ohms (Ω) of the fault loop comprising
@@ -24,3 +24,19 @@ def ptotection_rule():
     st.write("""*NOTE Where compliance with this subclause is provided by an RCD, the disconnecting times in 
     accordance with Table 41.1 relate to prospective residual fault currents significantly higher than 
     the rated residual operating current of the RCD (typically 5 I∆n).*""")
+
+    st.markdown("""<b>411.4.5</b> In TN systems, the following protective devices may be used for fault protection
+(protection against indirect contact):
+
+– overcurrent protective devices;
+
+– residual current protective devices (RCDs).
+
+<i>NOTE 1 Where an RCD is used for fault protection the circuit should also be protected by an overcurrent
+protective device in accordance with IEC 60364-4-43.
+A residual current protective device (RCD) shall not be used in TN-C systems.
+Where an RCD is used in a TN-C-S system, a PEN conductor shall not be used on the load
+side. The connection of the protective conductor to the PEN conductor shall be made on the
+source side of the RCD.
+
+NOTE 2 Where discrimination between RCDs is necessary, see 535.3 of IEC 60364-5-53.</i>""", unsafe_allow_html=True)
